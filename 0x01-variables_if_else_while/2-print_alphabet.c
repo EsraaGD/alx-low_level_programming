3faa71d94a064d5209ee238da8782612d44473f8
EsraaGD <isra.hatim2@gmail.com>
2023-08-11 14:28:53 -0700

    Vars

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: eachtime 0 (Success)
+ */
+
+int main()
+{
+	char f;
+	f = 'a';
+
+	for(f = 'a' ; f <= 'z' ; f++) {
+		putchar(f);
+	}
+
+	return (0);
+}
