3a0edccb982d0a42d27234dba652c46cd18e235f
EsraaGD <isra.hatim2@gmail.com>
2023-08-11 16:16:22 -0700

    Vars

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - all except q and e
+ * Return: eachtime 0 (Success)
+ */
+int main(void)
+{
+	char h;
+
+	for (h = 'a'; h <= 'z'; h++)
+	{
+		if (h != 'q' && h!= 'e')
+		{
+			putchar(h);
+		}
+	}
+
+	putchar ('\n');
+
+	return (0);
+}
