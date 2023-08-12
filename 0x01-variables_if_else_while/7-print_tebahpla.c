cd05eef8fdf53a794b89dd8fd48a7fd3aab3c6cd
EsraaGD <isra.hatim2@gmail.com>
2023-08-11 17:15:55 -0700

    Vars

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: eachtime 0 (Success)
+ */
+
+int main(void)
+{
+	char q;
+
+	q = 'z';
+	while
+		(q >= 'a')
+		{putchar (q); q--; }
+
+	putchar('\n');
+
+	return (0);
+}
