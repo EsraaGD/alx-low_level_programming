da419391dcd6c523ecfbc1680b5e01b9c66f407c
EsraaGD <isra.hatim2@gmail.com>
2023-08-15 12:43:27 -0700

    Nested loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - entry point
+ * Description - Print alphabet in lowercase
+ * Return: eachtime 0 (success)
+ */
+
+void print_alphabet(void);
+{
+	char z;
+
+	for(z = 'a'; z <= 'z'; z++)
+		_putchar(z);
+
+	_putchar('\n');
+}
