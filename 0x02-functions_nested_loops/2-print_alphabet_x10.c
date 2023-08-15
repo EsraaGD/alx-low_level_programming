51bc39089f5f2241c89fa681630d26004a834c82
EsraaGD <isra.hatim2@gmail.com>
2023-08-15 14:02:52 -0700

    Nested loops

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * Description: Print alphabets 10 times
+ *
+ * Return: eachtime 0 (Success)
+ */
+
+void print_alphabet_x10(void)
+{
+	char r;
+	int s;
+	s = 0;
+	while (s < 10)
+
+	{
+		for (r = 'a'; r <= 'z'; r++)
+
+		{
+			_putchar(r);
+		}
+		_putchar('\n');
+		s++;
+	}
+}
