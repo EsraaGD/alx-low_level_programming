7c9bef002c9ccb891eb51a3ca23f1d69f05c73c1
EsraaGD <isra.hatim2@gmail.com>
2023-08-16 06:48:45 -0700

    Vars adv

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -21,7 +21,7 @@ int main(void)
 					putchar(hundreds);
 					putchar(tens);
 					putchar(ones);
-					if (hundreds != '9' || tens == '8')
+					if (hundreds != '9' || tens != '8')
 					{
 						putchar(',');
 						putchar(' ');
