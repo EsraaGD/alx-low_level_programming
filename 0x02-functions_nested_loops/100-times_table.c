fa50477ac07dd8e038e9cfd2c25f6d8b3306b0a6
EsraaGD <isra.hatim2@gmail.com>
2023-08-16 15:40:49 -0700

    Nested loop

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,7 +7,7 @@
 
 void print_times_table(int w)
 {
-	int p ,q ,r;
+	int p, q, r;
 
 	if (w >= 0 && w <= 15)
 	{
@@ -19,21 +19,21 @@ void print_times_table(int w)
 				if (q == 0)
 				{
 					_putchar(r + '0');
-				}else if (r < 10  && q != 0)
+				} else if (r < 10  && q != 0)
 				{
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
 					_putchar(' ');
 					_putchar(r + '0');
-				}else if (r >= 10 && r < 100)
+				} else if (r >= 10 && r < 100)
 				{
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
 					_putchar((r / 10) + '0');
 					_putchar((r % 10) + '0');
-				}else if (r >= 100)
+				} else if (r >= 100)
 				{
 					_putchar(',');
 					_putchar(' ');
