f22ba9ac5696cd9594523861707073b3e6bbda91
EsraaGD <isra.hatim2@gmail.com>
2023-08-17 11:35:22 -0700

    Isprint

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,1 +1,16 @@
 #include "main.h"
+
+/**
+ * mul - multiplies two integers
+ * @u: Integer 1
+ * @v: Integer 2
+ * Return: eachtime 0 (success)
+ */
+
+int mul(int u, int v);
+
+{
+	int answer = u * v;
+
+	return (answer);
+}
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints the numbers from 0 to 9
+ * Return: eachtime (0)
+ */
+
+void print_numbers(void)
+{
+	char q;
+	for (q = '0'; q <= '9'; q++)
+	{
+		_putchar(q);
+	}
+	_putchar('\n');
+}
