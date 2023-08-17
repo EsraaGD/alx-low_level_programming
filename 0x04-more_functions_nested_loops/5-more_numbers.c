d0670f85a4c1c2f540f0365f193a7544c59ecb3f
EsraaGD <isra.hatim2@gmail.com>
2023-08-17 12:09:43 -0700

    Is 10

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * more_numbers - print 0 to 14, 10 times
+ * Return: 0
+ */
+
+void more_numbers(void)
+{
+	int s, r;
+
+	for (s = 1; s <= 10; s++)
+	{
+		for (r =0; r <= 14; r++)
+		{
+		if (r >= 10)
+		_putchar('1');
+		_putchar(r % 10 + '0');
+		}
+		_putchar('\n');
+	}
+}
