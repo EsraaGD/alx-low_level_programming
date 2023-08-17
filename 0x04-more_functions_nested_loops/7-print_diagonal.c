ded9f491a22255cc66ad986284504674b25408a9
EsraaGD <isra.hatim2@gmail.com>
2023-08-17 12:49:36 -0700

    Diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_diagonal - draws a diagonal line
+ * @n: Number of times
+ * Return: 0 
+ */
+
+void print_diagonal(int c)
+
+{
+	int d, e;
+
+	if (c <= 0)
+		_putchar('\n');
+	for (d = 0; d < c; d++)
+	{
+		for (e = 0 ; e < d; e++)
+		{
+			_putchar(' ');
+		}
+		_putchar('\\');
+		_putchar('\n');
+	}
+}
