eebb74da158e7fae15caaf12594723cd5901299b
EsraaGD <isra.hatim2@gmail.com>
2023-09-01 12:04:32 -0700

    Task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - a program that prints its name
+ * @argc: number of command line arguments
+ * @argv: array
+ * Return: 0
+ */
+
+int main(int argc, char* argv[])
+{
+	(void)argc;
+
+	printf("%s\n", argv [0]);
+	return (0);
+}
