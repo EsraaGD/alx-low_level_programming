74b01997e60169ced0af07b986f18e99ae31c482
EsraaGD <isra.hatim2@gmail.com>
2023-09-07 11:22:20 -0700

    Task 4

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 /**
- * *_realloc - a function that reallocates a memory block using malloc and free.
+ * *_realloc - a function that reallocates a memory block using malloc and free
  * @ptr: pointer to the memory previously allocated.
  * @old_size: size, in bytes, of the allocated space of ptr.
  * @new_size: new size, in bytes, of the new memory block.
