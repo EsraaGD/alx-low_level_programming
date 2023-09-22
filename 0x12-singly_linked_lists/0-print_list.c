df1ebbcad56e013af619528472406f406d569ae9
EsraaGD <isra.hatim2@gmail.com>
2023-09-22 09:19:54 -0700

    SSL 0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/*
+ * print_list - prints all the elements of a list_t list
+ * @d: input
+ * Return: 0
+ */
+
+size_t print_list(const list_t *d)
+{
+	size_t linkylinks;
+
+	linkylinks = 0;
+	while (d != NULL)
+	{
+		if (d->str == NULL)
+			printf("[%d] %s\n", 0, "(nil)");
+		else
+			printf("[%d] %s\n", d->len, d->str);
+		d = d->next;
+		linkylinks++;
+	}
+	return (linkylinks);
+}
