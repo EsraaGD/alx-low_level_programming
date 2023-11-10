2ede0218fc6c17ef176b5a0bb35c7c7885b31aad
EsraaGD <isra.hatim2@gmail.com>
2023-11-10 08:57:22 -0800

    Task 7 and 8

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -9,28 +9,32 @@
 
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
+	dlistint_t *current = *head;
+	unsigned int i;
+
 	if (*head == NULL)
 		return (-1);
 
-	dlistint_t *current = *head;
-
 	if (index == 0)
 	{
-		*head = current->next;
+		*head = (*head)->next;
 		if (*head != NULL)
+		{
 			(*head)->prev = NULL;
+		}
 		free(current);
 		return (1);
 	}
 
-	for (unsigned int i = 0; current != NULL && i < index; i++)
+	for (i = 0; current != NULL && i < index; i++)
+	{
 		current = current->next;
+	}
 
 	if (current == NULL)
 		return (-1);
 
-	if (current->prev != NULL)
-		current->prev->next = current->next;
+	current->prev->next = current->next;
 
 	if (current->next != NULL)
 		current->next->prev = current->prev;
