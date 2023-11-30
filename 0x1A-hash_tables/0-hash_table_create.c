8b5a624e975d86d7d15f4f28c7354428f67275d5
Isra Hatim <138698238+EsraaGD@users.noreply.github.com>
2023-11-30 19:57:02 +0300

    Task 0

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,26 @@
+#include "hash_tables.h"
+/**
+ * hash_table_create - creates a hash table
+ * @size:size of array
+ *
+ * Return: ptr to new hash table or NULL if something went wrong
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+    hash_table_t *htc = malloc(sizeof(hash_table_t));
+
+    if (htc == NULL)
+        return (NULL);
+
+    htc->size = size;
+    ht->array = malloc(size * sizeof(hash_node_t *));
+
+    if (htc->array == NULL)
+        return (NULL);
+
+    for (int i = 0; i < size; i++)
+        htc->array[i] = NULL;
+
+    return (htc);
+}
