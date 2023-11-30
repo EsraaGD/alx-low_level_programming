f4a2e8dbb139e558dcbe7a312b99af544818864e
EsraaGD <isra.hatim2@gmail.com>
2023-11-30 10:42:12 -0800

    Task 3 done

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,9 +1,9 @@
 #include "hash_tables.h"
 /**
  * hash_table_set - adds an element to the hash table
- * @ht:
- * @key:
- * @value:
+ * @ht: the hash table you want to add or update the key/value to
+ * @key: is the key, cannot be an empty string
+ * @value: is the value associated with the key
  *
  * Return: 1 if it succeeded, 0 otherwise
  * In case of collision, add the new node at the beginning of the list
@@ -11,33 +11,36 @@
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-    if (ht == NULL || key == NULL || *key == '\0')
-        return 0;
+	unsigned long int index;
+	hash_node_t *new_node;
 
-    unsigned long int index = key_index((unsigned char *)key, ht->size);
-    hash_node_t *new_node = malloc(sizeof(hash_node_t));
+	if (ht == NULL || key == NULL || *key == '\0')
+		return (0);
 
-    if (new_node == NULL)
-        return 0;
+	index = key_index((unsigned char *)key, ht->size);
+	new_node = malloc(sizeof(hash_node_t));
 
-    new_node->key = strdup(key);
-    if (new_node->key == NULL)
-    {
-        free(new_node);
-        return 0;
-    }
+	if (new_node == NULL)
+		return (0);
 
-    new_node->value = strdup(value);
-    if (new_node->value == NULL)
-    {
-        free(new_node->key);
-        free(new_node);
-        return 0;
-    }
+	new_node->key = strdup(key);
+	if (new_node->key == NULL)
+	{
+		free(new_node);
+		return (0);
+	}
 
-    /* Handle collision by adding the new node at the beginning of the list */
-    new_node->next = ht->array[index];
-    ht->array[index] = new_node;
+	new_node->value = strdup(value);
+	if (new_node->value == NULL)
+	{
+		free(new_node->key);
+		free(new_node);
+		return (0);
+	}
 
-    return 1;
+	/* Handle collision by adding the new node at the beginning of the list */
+	new_node->next = ht->array[index];
+	ht->array[index] = new_node;
+
+	return (1);
 }
