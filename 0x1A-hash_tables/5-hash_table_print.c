e172b53665dbb08f783eac60e0c50351d49cc9fd
EsraaGD <isra.hatim2@gmail.com>
2023-11-30 13:43:36 -0800

    Task 5

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -7,25 +7,27 @@
  * Return: Print hash table
  */
 
-void hash_table_print(const hash_table_t *ht);
+void hash_table_print(const hash_table_t *ht)
 {
-    if (ht == NULL)
-        return;
+	unsigned long int i;
 
-    printf("{");
+	if (ht == NULL)
+		return;
 
-    for (unsigned long int i = 0; i < ht->size; i++)
-    {
-        hash_node_t *current = ht->array[i];
+	printf("{");
 
-        while (current != NULL)
-        {
-            printf("'%s': '%s'", current->key, current->value);
-            current = current->next;
+	for (i = 0; i < ht->size; i++)
+	{
+		hash_node_t *current = ht->array[i];
 
-            if (current != NULL)
-                printf(", ");
-        }
-    }
-    printf("}\n");
+		while (current != NULL)
+		{
+			printf("'%s': '%s'", current->key, current->value);
+			current = current->next;
+
+			if (current != NULL)
+				printf(", ");
+		}
+	}
+	printf("}\n");
 }
