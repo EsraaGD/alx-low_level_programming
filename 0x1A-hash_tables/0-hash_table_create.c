3cf805c5f4419178f742fd8c7c57cfefb77d71fe
Isra Hatim <138698238+EsraaGD@users.noreply.github.com>
2023-11-30 20:22:53 +0300

    Task 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,7 +9,6 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
     hash_table_t *htc = malloc(sizeof(hash_table_t));
-    unsigned long int h;
 
     if (htc == NULL)
         return (NULL);
