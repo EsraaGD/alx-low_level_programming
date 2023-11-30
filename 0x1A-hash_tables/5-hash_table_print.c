abc5ef049d1cc0c512eb212d5e439ebab77acf07
Isra Hatim <138698238+EsraaGD@users.noreply.github.com>
2023-11-30 21:58:33 +0300

    Task 4 and 5

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - a function that prints a hash table.
+ * @ht: is the hash table
+ *
+ * Return: Print hash table
+ */
+
+void hash_table_print(const hash_table_t *ht);
+{
+    if (ht == NULL)
+        return;
+
+    printf("{");
+
+    for (unsigned long int i = 0; i < ht->size; i++)
+    {
+        hash_node_t *current = ht->array[i];
+
+        while (current != NULL)
+        {
+            printf("'%s': '%s'", current->key, current->value);
+            current = current->next;
+
+            if (current != NULL)
+                printf(", ");
+        }
+    }
+    printf("}\n");
+}
