7e6e2ae135cc3d6b2cd1310cbe9eecbec087ce61
EsraaGD <isra.hatim2@gmail.com>
2024-03-14 02:33:46 -0700

    T1 done ?

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -7,7 +7,7 @@
  * @size: number of elements in array
  * @value: value to search for
  * Return: the first index where value is located, or -1 if value is not
- * present in array or if array is NULL
+ * present in array or if array is NULLL
  */
 
 int binary_search(int *array, size_t size, int value)
