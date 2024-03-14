dd1e8f9dfe220b4833cdae71a33bce13de91a93f
Isra Hatim <138698238+EsraaGD@users.noreply.github.com>
2024-03-14 13:53:50 +0300

    T1 last fix

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -14,7 +14,7 @@ int binary_search(int *array, size_t size, int value)
 	int left, right, mid, i;
 
 	if (array == NULL || size == 0)
-		return -1;
+		return (-1);
 
 	left = 0;
 	right = size - 1;
@@ -33,12 +33,12 @@ int binary_search(int *array, size_t size, int value)
 		mid = left + (right - left) / 2;
 
 		if (array[mid] == value)
-			return mid;
+			return (mid);
 		else if (array[mid] < value)
 			left = mid + 1;
 		else
 			right = mid - 1;
 	}
 
-	return -1;
+	return (-1);
 }
