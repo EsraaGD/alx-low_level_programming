3e2e7899220a8350496ce5e922062cf3ef744bde
Isra Hatim <138698238+EsraaGD@users.noreply.github.com>
2024-03-14 12:28:32 +0300

    T1 fix

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 #include "search_algos.h"
 
+/**
+ * binary_search - searches for a value in an array of integers using BSA
+ * @array: pointer to the first element of the array to search in
+ * @size: number of elements in array
+ * @value: value to search for
+ * Return: the first index where value is located, or -1 if value is not
+ * present in array or if array is NULL
+ */
+
 int binary_search(int *array, size_t size, int value)
 {
 	int left, right, mid, i;
